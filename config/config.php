f6b696a4ef77b82bc161998577ba5fb1fe4a21b8
Nero-dante <95326701+Nero-dante@users.noreply.github.com>
2022-08-16 17:05:44 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,10 +2,10 @@
 //RENAME TO "config.php"
 function con()
 {
-    $servername = "";
-    $username = "";
-    $password = "";
-    $db = "";
+    $servername = "x71wqc4m22j8e3ql.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $username = "pb1s9tfx62isn5hq";
+    $password = "bi8jpd4c99i5r0wz";
+    $db = "zkoqblypztcxg7le";
     $con = new mysqli($servername, $username, $password, $db);
     if ($con->connect_error) {
         die("Connection failed: " . $con->connect_error);
